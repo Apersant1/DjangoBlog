67c8e74310f4efa6421d78b1ce9b517875a9c745
Apersant1 <miracles.on.the.turn@gmail.com>
2020-11-15 18:09:09 +0300

    Init project

---
+++ Blog/views.py
@@ -0,0 +1,14 @@
+from django.views.generic import ListView,DetailView,CreateView
+from .models import Post
+class BlogListView(ListView):
+    model = Post
+    template_name = "home.html"
+
+class BlogDetailView(DetailView): # новое
+    model = Post
+    template_name = 'post_detail.html'
+
+class BlogCreateView(CreateView):
+    model = Post
+    fields = ['title','author','body']
+    template_name = 'create post.html'

c395f07eda7270ec5a8119c68efcc63c671e7410
Apersant1 <miracles.on.the.turn@gmail.com>
2020-11-16 16:11:56 +0300

    Added Remove func

--- Blog/views.py
+++ Blog/views.py
@@ -1,23 +1,33 @@
-from django.views.generic import ListView,DetailView,CreateView,UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Post
+
+
 class BlogListView(ListView):
     model = Post
     template_name = "home.html"
 
-class BlogDetailView(DetailView): # новое
+
+class BlogDetailView(DetailView):  # новое
     model = Post
     template_name = 'post_detail.html'
 
+
 class BlogCreateView(CreateView):
     model = Post
-    fields = ['title','author','body']
+    fields = ['title', 'author', 'body']
     template_name = 'create post.html'
+
+
 #
-# class BlogDeleteView(DeleteView):
-#     pass
 
 class BlogUpdateView(UpdateView):
     model = Post
-    fields = ['title','body']
+    fields = ['title', 'body']
     template_name = 'post_edit.html'
-# TODO: need create deletePost view!
+
+
+class BlogDeleteView(DeleteView):  # Создание нового класса
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
